54865160dac5b02f620bd87196c418da1f7d8a37
sairamdammalapati <sairamdammalapati@gmail.com>
2019-08-10 12:33:07 +0530

    Change Related to api change

--- table8_BE/src/main/java/com/dbs/hacktron/controller/HacktronController.java
+++ table8_BE/src/main/java/com/dbs/hacktron/controller/HacktronController.java
@@ -4,6 +4,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,13 @@ public class HacktronController {
 		
 	}
 	
-	@GetMapping("/queue/")
+	@GetMapping("/queue")
 	public void getQueue() {
 		
 	}
 	
-	@DeleteMapping("/queue/")
-	public void deleteQueue(@PathParam(value = "queueId") int queueId) {
+	@DeleteMapping("/queue/{queueId}")
+	public void deleteQueue(@PathVariable int queueId) {
 		
 	}
 }
