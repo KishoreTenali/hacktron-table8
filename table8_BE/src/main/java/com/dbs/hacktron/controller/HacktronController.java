12f6836f1c8f7def512d84db032c4bc2e543261d
nishantarora25 <53960102+nishantarora25@users.noreply.github.com>
2019-08-10 14:07:25 +0530

    Handling Exceptions

--- table8_BE/src/main/java/com/dbs/hacktron/controller/HacktronController.java
+++ table8_BE/src/main/java/com/dbs/hacktron/controller/HacktronController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dbs.hacktron.exception.ResourceNotFoundException;
 import com.dbs.hacktron.utils.Queue;
+
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -38,23 +40,30 @@ public class HacktronController {
 	}
 	
 	@GetMapping("/queue")
-	public @ResponseBody Map<String,Queue> getQueueList() {
+	public @ResponseBody Map<String,Queue> getQueueList() throws ResourceNotFoundException {
 		
+		if(queues.isEmpty())
+		{
+			throw new ResourceNotFoundException("No QUEUES Found");
+		}
 		return queues;
 	}
 	
 	@DeleteMapping("/queue/{queueId}")
-	public String deleteQueue(@PathVariable("queueId") String queueId) {
+	public String deleteQueue(@PathVariable("queueId") String queueId) throws ResourceNotFoundException {
+		if(queues.get(queueId).equals(null)){
+			throw new ResourceNotFoundException("invalid QueueId");
+		}
 		queues.remove(queueId);
-		
 		return "Queue " + queueId + " Removed Successfully";
 	}
 	
 	@GetMapping("/queue/{id}/message")
-	public LinkedList<String> getAllMessages(@PathVariable(value = "id") String queueId) {
-		
+	public LinkedList<String> getAllMessages(@PathVariable(value = "id") String queueId) throws ResourceNotFoundException {
+		if(queues.get(queueId).equals(null)){
+			throw new ResourceNotFoundException("invalid QueueId");
+		}
 		Queue queue = queues.get(queueId);
-		
 		return queue.getData();
 		
 	}
@@ -63,9 +72,11 @@ public class HacktronController {
 
 	@DeleteMapping("/queue/{id}/message/")
 	public String deleteMessageByQueueId(@PathVariable(value = "id") String queueId,
-			@PathVariable(value = "id") String messageId) {
+			@PathVariable(value = "id") String messageId) throws ResourceNotFoundException {
+		if(queues.get(queueId).equals(null)){
+			throw new ResourceNotFoundException("invalid QueueId");
+		}
 		queues.get(queueId).getData().poll();
-		
 		return queues.get(queueId).getData().getFirst() + " is removed ";
 	}
 }
