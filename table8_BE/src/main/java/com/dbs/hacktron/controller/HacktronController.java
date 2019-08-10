5b2f138670407f8f09182ef6b9eddde4ef965169
sairamdammalapati <sairamdammalapati@gmail.com>
2019-08-10 12:11:59 +0530

    Added Queue endpoints and service interface
    Added Queue endpoints and service interface

--- table8_BE/src/main/java/com/dbs/hacktron/controller/HacktronController.java
+++ table8_BE/src/main/java/com/dbs/hacktron/controller/HacktronController.java
@@ -1,5 +1,29 @@
 package com.dbs.hacktron.controller;
 
-public class HacktronController {
+import javax.websocket.server.PathParam;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HacktronController {
+	
+	@PostMapping("/queue/create")
+	public void addQueue(@PathParam(value = "queName") String queName) {
+		
+	}
+	
+	@GetMapping("/queue/")
+	public void getQueue() {
+		
+	}
+	
+	@DeleteMapping("/queue/")
+	public void deleteQueue(@PathParam(value = "queueId") int queueId) {
+		
+	}
 }
---
+++ table8_BE/src/main/java/com/dbs/hacktron/service/IQueueService.java
@@ -0,0 +1,9 @@
+package com.dbs.hacktron.service;
+
+public interface IQueueService {
+	
+	public void addQueue(String queName);
+	public void getQueue();
+	public void deleteQueue(int queueId);
+
+}
