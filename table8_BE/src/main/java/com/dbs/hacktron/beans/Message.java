df5f537225717d2d8d9eaf4ecb04f455686a8d5a
naveen <regantinaveen@gmail.com>
2019-08-10 12:39:15 +0530

    Added Beans package

---
+++ table8_BE/src/main/java/com/dbs/hacktron/beans/Message.java
@@ -0,0 +1,26 @@
+package com.dbs.hacktron.beans;
+
+public class Message {
+
+	private int id;
+	private String name;
+	private String data;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+}
